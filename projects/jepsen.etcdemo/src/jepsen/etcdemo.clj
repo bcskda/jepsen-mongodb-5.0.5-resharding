5eb7d2bac573bd86e8a208546d96e8913b812fad
bcskda <demulkup@ya.ru>
2021-12-02 18:37:37 +0300

    up

--- projects/jepsen.etcdemo/src/jepsen/etcdemo.clj
+++ projects/jepsen.etcdemo/src/jepsen/etcdemo.clj
@@ -1,18 +1,31 @@
 (ns jepsen.etcdemo
-  (:require [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+                    [control :as c]
+                    [db :as db]
+                    [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn db
+  "Etcd for this version"
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing etcd" version))
+    (teardown! [_ test node]
+      (info node "tearing down etcd" version))))
 
 (defn etcd-test
   "Options -> test map"
   [opts]
   (merge tests/noop-test
-         {:pure-generators true}
-         opts))
+         opts
+         {:name            "etcd"
+          :os              debian/os
+          :db              (db "v3.1.5")
+          :pure-generators true}))
 
 (defn -main
   "Handles cmdline. Can run a test or a webserver to observe results"
