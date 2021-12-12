c4e81145304b8ff7c679038d705d90d14c966c31
bcskda <demulkup@ya.ru>
2021-12-12 16:37:17 +0300

    mongodb5: lein scaffolding

---
+++ projects/jepsen.mongodb5/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mongodb5
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ projects/jepsen.mongodb5/test/jepsen/mongodb5_test.clj
@@ -0,0 +1,7 @@
+(ns jepsen.mongodb5-test
+  (:require [clojure.test :refer :all]
+            [mongodb5.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
