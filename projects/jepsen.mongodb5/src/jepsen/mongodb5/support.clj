19ab278d87eb22cabcff4e208a396fdeaee54048
bcskda <demulkup@ya.ru>
2022-03-21 10:07:19 +0300

    Generate mongod config

--- projects/jepsen.mongodb5/src/jepsen/mongodb5/support.clj
+++ projects/jepsen.mongodb5/src/jepsen/mongodb5/support.clj
@@ -1,5 +1,6 @@
 (ns jepsen.mongodb5.support
-  (:require [clojure.tools.logging :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :refer :all]
             [clojure.string :as str]
             [jepsen [control :as c]
                     [db :as db]]
@@ -10,9 +11,9 @@
 
 (def mongodb-prefix "/opt/mongodb5")
 
-(def mongod-binary (str mongodb-prefix "/bin/mongod"))
+(def mongod-binary-path (str mongodb-prefix "/bin/mongod"))
 
-(def mongod-config (str mongodb-prefix "/mongod.custom.conf"))
+(def mongod-config-path (str mongodb-prefix "/mongod.custom.conf"))
 
 (def mongod-logfile (str mongodb-prefix "/log/mongod.log"))
 
@@ -20,7 +21,7 @@
 
 (def mongosh-prefix "/opt/mongosh")
 
-(def mongosh-binary (str mongosh-prefix "/bin/mongo"))
+(def mongosh-binary-path (str mongosh-prefix "/bin/mongo"))
 
 (defn tarball-url
   "URL for Debian 10 tarball"
@@ -57,21 +58,35 @@
 
 (defn configure-mongod
   "Install mongod config file and create directories"
-  [local-config-path]
+  [rs-name]
   (c/su
     (let [data-directory (str mongodb-prefix "/data")
-          log-directory (str mongodb-prefix "/log")]
-      (c/upload local-config-path mongod-config)
+          log-directory (str mongodb-prefix "/log")
+          config-template (slurp (io/resource "mongod.conf"))
+          config (format config-template rs-name)]
+      (cu/write-file! config mongod-config-path)
       (prepare-dir data-directory)
       (prepare-dir log-directory))))
 
+(defn replica-set-members-js
+  "String with JS array to supply as `members` to `rs.initiate`"
+  [nodes]
+  (let [node-ids (range (count nodes))
+        mapper (fn [id n] (format "{_id: %d, host: \"%s\"}" id n))
+        nodes-js (map mapper node-ids nodes)]
+    (str "["
+         (clojure.string/join "," nodes-js)
+         "]")))
+
 (defn replica-set-initiate
   "Initiate replica-set (call from only one node)"
-  [rs-name]
-  (let [remote-script-path (str (cscp/tmp-file) ".js")
-        local-script-path (str "resources/rs-init--" rs-name".js")]
-    (c/upload local-script-path remote-script-path)
-    (c/exec mongosh-binary "localhost" remote-script-path)))
+  [rs-name rs-nodes]
+  (let [remote-script-path (str (cu/tmp-file!))
+        script-template (slurp (io/resource "rs-init.js"))
+        rs-nodes-js (replica-set-members-js rs-nodes)
+        script (format script-template rs-name rs-nodes-js)]
+    (cu/write-file! script remote-script-path)
+    (c/exec mongosh-binary-path "localhost" remote-script-path)))
 
 (defn db
   "Mongo for this specific version"
@@ -82,24 +97,24 @@
       (let [config-path "resources/mongod-replicated-no-sharding.conf"]
         (c/su
           (install-mongodb version)
-          (configure-mongod config-path)
+          (configure-mongod rs-name)
           (cu/start-daemon!
             {:logfile mongod-logfile
             :pidfile mongod-pidfile
             :chdir mongodb-prefix}
-            mongod-binary
-            :--config mongod-config)
+            mongod-binary-path
+            :--config mongod-config-path)
           (Thread/sleep 20000)
           (when (= node "n1")
             (do
               (install-mongosh version)
-              (replica-set-initiate rs-name)
+              (replica-set-initiate rs-name (:nodes test))
               (Thread/sleep 20000))))))
 
     (teardown! [_ test node]
       (info node "tearing down mongo" version)
       (c/su
-        (cu/stop-daemon! mongod-binary mongod-pidfile)
+        (cu/stop-daemon! mongod-binary-path mongod-pidfile)
         (c/exec :rm :-rf mongodb-prefix)
         (c/exec :rm :-rf mongosh-prefix)))
 
