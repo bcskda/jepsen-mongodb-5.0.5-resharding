1a11e9928a43a22288e169999aa682f5a782e2f6
Dmitrii Kuptsov <demulkup@ya.ru>
2022-05-04 23:14:42 +0300

    introduce sharding, keep compat

--- projects/jepsen.mongodb5/src/jepsen/mongodb5/support.clj
+++ projects/jepsen.mongodb5/src/jepsen/mongodb5/support.clj
@@ -11,6 +11,7 @@
 
 (def mongodb-prefix "/opt/mongodb5")
 
+
 (def mongod-binary-path (str mongodb-prefix "/bin/mongod"))
 
 (def mongod-config-path (str mongodb-prefix "/mongod.custom.conf"))
@@ -18,6 +19,14 @@
 (def mongod-logfile (str mongodb-prefix "/log/mongod.log"))
 
 (def mongod-pidfile (str mongodb-prefix "/mongod.pid"))
+
+
+(def mongos-binary-path (str mongodb-prefix "/bin/mongos"))
+
+(def mongos-config-path (str mongodb-prefix "/mongos.custom.conf"))
+
+(def mongos-pidfile (str mongodb-prefix "/mongos.pid"))
+
 
 (def mongosh-prefix "/opt/mongosh")
 
@@ -56,37 +65,100 @@
                  ".tgz")]
     (cu/install-archive! url mongosh-prefix)))
 
-(defn configure-mongod
-  "Install mongod config file and create directories"
+(defn configure-mongod-config
   [rs-name]
   (c/su
     (let [data-directory (str mongodb-prefix "/data")
           log-directory (str mongodb-prefix "/log")
-          config-template (slurp (io/resource "mongod.conf"))
+          config-template (slurp (io/resource "mongod-non-sharded.conf"))
           config (format config-template rs-name)]
       (cu/write-file! config mongod-config-path)
       (prepare-dir data-directory)
       (prepare-dir log-directory))))
 
+(defn configure-mongod-shard
+  [rs-name]
+  (c/su
+    (let [data-directory (str mongodb-prefix "/data")
+          log-directory (str mongodb-prefix "/log")
+          config-template (slurp (io/resource "mongod-sharded.conf"))
+          config (format config-template rs-name)]
+      (cu/write-file! config mongod-config-path)
+      (prepare-dir data-directory)
+      (prepare-dir log-directory))))
+
+(defn configure-mongod
+  "Install mongod config file and create directories"
+   ([rs-name is-shard]
+    (if is-shard
+        (configure-mongod-shard rs-name)
+        (configure-mongod-config rs-name)))
+   ([rs-name]
+    (configure-mongod rs-name false)))
+
+(defn configure-mongos
+  "Install mongos config file and create directories"
+   [configsvr-rs configsvr-nodes]
+   (c/su
+     (let [config-template (slurp (io/resource "mongos.conf"))
+          config (format config-template configsvr-rs configsvr-nodes)]
+       (cu/write-file! config mongos-config-path))))
+
+
 (defn replica-set-members-js
   "String with JS array to supply as `members` to `rs.initiate`"
   [nodes]
-  (let [node-ids (range (count nodes))
+  (let [node-ids (map #(Integer. (subs % 1)) nodes)
         mapper (fn [id n] (format "{_id: %d, host: \"%s\"}" id n))
         nodes-js (map mapper node-ids nodes)]
     (str "["
          (clojure.string/join "," nodes-js)
          "]")))
 
-(defn replica-set-initiate
+(defn replica-set-initiate-sharded
+  [rs-name rs-nodes is-shard]
+  (let [remote-script-path (str (cu/tmp-file!))
+        script-template (slurp (io/resource "rs-init-sharded.js"))
+        rs-nodes-js (replica-set-members-js rs-nodes)
+        is-configsvr (not is-shard)
+        script (format script-template rs-name rs-nodes-js is-configsvr)]
+    (cu/write-file! script remote-script-path)
+    (c/exec mongosh-binary-path "localhost" remote-script-path)))
+
+(defn replica-set-initiate-non-sharded
   "Initiate replica-set (call from only one node)"
   [rs-name rs-nodes]
   (let [remote-script-path (str (cu/tmp-file!))
-        script-template (slurp (io/resource "rs-init.js"))
+        script-template (slurp (io/resource "rs-init-non-sharded.js"))
         rs-nodes-js (replica-set-members-js rs-nodes)
         script (format script-template rs-name rs-nodes-js)]
     (cu/write-file! script remote-script-path)
     (c/exec mongosh-binary-path "localhost" remote-script-path)))
+
+(defn replica-set-initiate
+  "Initiate replica-set (call from only one node)"
+  ([rs-name rs-nodes]
+   (replica-set-initiate-non-sharded rs-name rs-nodes))
+  ([rs-name rs-nodes is-shard]
+   (replica-set-initiate-sharded rs-name rs-nodes is-shard)))
+
+
+(defn add-shard
+  [nodes rs-name]
+  (let [remote-script-path (str (cu/tmp-file!))
+        script-template (slurp (io/resource "add-shard.js"))
+        script (format script-template rs-name nodes)]
+    (cu/write-file! script remote-script-path)
+    (c/exec mongosh-binary-path "localhost" remote-script-path)))
+
+(defn add-all-shards
+  [all-nodes rs-basename]
+  (let [shards-count (- (quot (count all-nodes) 3) 1)]
+    (dotimes [shard-number shards-count]
+      (let [shard-number (+ shard-number 1)
+            shard-nodes (map #(str "n" (+ % (* 3 shard-number))) [1 2 3])
+            shard-rs-name (str rs-basename "-shard" shard-number)]
+        (add-shard shard-nodes shard-rs-name)))))
 
 (defn db
   "Mongo for this specific version"
@@ -94,7 +166,21 @@
   (reify db/DB
     (setup! [_ test node]
       (info node "installing mongo" version)
-      (let [config-path "resources/mongod-replicated-no-sharding.conf"]
+      (let [config-path "resources/mongod-replicated-no-sharding.conf"
+            node-number (Integer. (subs node 1))
+            shard-number (quot (- node-number 1) 3)
+            rs-nodes (if (:sharded test)
+                         (map #(str "n" (+ % (* 3 shard-number))) [1 2 3])
+                         (:nodes test))
+            is-rs-initiator (if (:sharded test)
+                                (= (mod node-number 3) 1)
+                                (= node-number 1))
+            is-shard (> shard-number 0)
+            confrs-name (str rs-name "-conf")
+            shardrs-name (str rs-name "-shard" shard-number)
+            rs-name (if (:sharded test)
+                        (if is-shard shardrs-name confrs-name)
+                        rs-name)]
         (c/su
           (install-mongodb version)
           (configure-mongod rs-name)
@@ -105,15 +191,32 @@
             mongod-binary-path
             :--config mongod-config-path)
           (Thread/sleep 20000)
-          (when (= node "n1")
+          (when (:sharded test)
+            (do
+              ;(install-mongos version)
+              (configure-mongos confrs-name ["n1" "n2" "n3"])
+              (cu/start-daemon!
+                {:pidfile mongos-pidfile
+                 :chdir mongodb-prefix}
+                 mongos-binary-path
+                 :--config mongos-config-path)
+                 )
+              (Thread/sleep 20000))
+          (when is-rs-initiator
             (do
               (install-mongosh version)
-              (replica-set-initiate rs-name (:nodes test))
-              (Thread/sleep 20000))))))
+              (if (:sharded test)
+                (replica-set-initiate rs-name rs-nodes is-shard)
+                (replica-set-initiate rs-name rs-nodes))
+              (Thread/sleep 20000)))
+          (when (and (:sharded test) (= node-number 0))
+            (add-all-shards (:nodes test) rs-name)))))
 
     (teardown! [_ test node]
       (info node "tearing down mongo" version)
       (c/su
+        (when (:sharded test)
+          (cu/stop-daemon! mongos-binary-path mongos-pidfile))
         (cu/stop-daemon! mongod-binary-path mongod-pidfile)
         (c/exec :rm :-rf mongodb-prefix)
         (c/exec :rm :-rf mongosh-prefix)))
