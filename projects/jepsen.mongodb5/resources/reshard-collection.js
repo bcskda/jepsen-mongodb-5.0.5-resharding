f2ead2c901d00c8e99464068c3799500a5115867
Dmitrii Kuptsov <demulkup@ya.ru>
2022-05-06 00:06:43 +0300

    Resharding as a testable db operation

---
+++ projects/jepsen.mongodb5/resources/reshard-collection.js
@@ -0,0 +1,18 @@
+const checkProgress = (ns) => db
+    .getSiblingDB("admin")
+    .aggregate([
+        { $currentOp: { allUsers: true, localOps: false } },
+        { $match: { type: "op", "originatingCommand.reshardCollection": ns } }
+    ]
+);
+
+
+const ns = "%s";
+const newKey = %s;
+const command = {reshardCollection: ns, key: newKey};
+
+const result = db.adminCommand(command);
+print("Result:");
+printjson(result);
+
+assert(result.ok === 1);
