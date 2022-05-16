525409969f1aa281c6dbc45104d7b5ad1b29a4c0
Dmitrii Kuptsov <demulkup@ya.ru>
2022-05-16 17:58:50 +0300

    cleaner test

--- projects/jepsen.mongodb5/src/jepsen/mongodb5/core.clj
+++ projects/jepsen.mongodb5/src/jepsen/mongodb5/core.clj
@@ -10,8 +10,7 @@
 
 (defn default-test-fn
   [opts]
-  ;(mongo-tests/single-shard-only-snapshot-isolation replica-set-name opts))
-  (mongo-tests/try-multishard-deployment replica-set-name opts))
+  (mongo-tests/resharding-survives-primary-failover replica-set-name opts))
 
 (defn all-test-fns
   [opts]
